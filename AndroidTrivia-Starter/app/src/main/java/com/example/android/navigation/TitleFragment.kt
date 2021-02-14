bedc86cbc2ac794df780422cc111ad0b101d8b85
June-Han <julialiu08@gmail.com>
2021-02-15 00:00:28 +0800

    kotlin fundamentals 03.2 navigation paths

---
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,42 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.title_fragment, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        //Create the menu with the specifications
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+        //Select
+        return NavigationUI.onNavDestinationSelected(
+                item, requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+
+}
